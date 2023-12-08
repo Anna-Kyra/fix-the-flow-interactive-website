d930f9fbb05320d74e0fecd1f9c13b8cd5b41d57
Anna-Kyra <anna-kyra.strik@hva.nl>
2023-12-08 09:33:15 +0100

    checkboxes

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,42 @@
-console.log('hi')
+// window.addEventListener('DOMContentLoaded', function(){
+//     var checkboxes=document.querySelectorAll('input[type="checkbox]')
+//     var maxAllowed=3;
+
+//     checkboxes.forEach(function(checkbox){
+//         checkbox.addEventListener('change',function(){
+//             var checkedCount=document.querySelectorAll('input[type="checkbox"]:checked').length;
+//             if(checkedCount>maxAllowed){
+//                 this.checked=false;
+//             }
+//             if(checkedCount==maxAllowed){
+//                 checkboxes.forEach(function(otherCheckbox){
+//                     if(!otherCheckbox.checked){
+//                         otherCheckbox.disabled=true;
+//                     }
+//                 });
+//                 updateLable("Maximum Selected Checkboxes Reached");
+//             }
+//             else{
+//                 checkboxes.forEach(function(otherCheckbox){
+//                     otherCheckbox.disabled=false;
+//                 });
+//                 updateLable("");
+//             }
+//         });
+//     });
+//     function updateLable(message){
+//         var label=document.getElementById('messagelabel');
+//         label.textContent=message;
+//     }
+// });
+
+function countCheckboxes(){
+    var a = document.forms["main"];
+    var x = a.querySelectorAll('input[type="checkbox"]:checked');
+    var choose = document.getElementById('choose');
+    choose.textContent= `Kies ${x.length} uit 5`;
+    // console.log(`Kies ${x.length} uit 5`);
+}
+// choose.textContent= x.length;
+
+console.log('hi');
